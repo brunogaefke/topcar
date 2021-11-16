95d59aad9e9f4035f6fa6d455b0b5cc8dc221545
Bruno Gaefke <brunogaefke@gmail.com>
2021-11-16 14:27:14 -0300

    Page Home created and stylized until at X Small

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import './styles.css';
+import MainImage from 'assets/images/car-home.png';
+import Navbar from 'components/Navbar';
+import ButtonIconHome from 'components/ButtonIconHome';
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-image-container">
+            <img src={MainImage} alt="ImageHome" /> 
+              <h1>O carro perfeito para você</h1>
+              <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+          </div>
+          </div>
+          <div className="home-card-search">
+          <div className="home-container-botton">
+              <ButtonIconHome/>
+              <p>Comece agora a navegar</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
