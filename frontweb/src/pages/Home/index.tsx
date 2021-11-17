08c8be41dccec418bb97b4ec48649e6bbe40f595
Bruno Gaefke <brunogaefke@gmail.com>
2021-11-16 22:13:06 -0300

    home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -12,10 +12,12 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-image-container">
-            <img src={MainImage} alt="ImageHome" /> 
+            <img src={MainImage} alt="ImageHome" />
+            <div className="home-text-container">
               <h1>O carro perfeito para você</h1>
               <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
-          </div>
+              </div>
+              </div> 
           </div>
           <div className="home-card-search">
           <div className="home-container-botton">
